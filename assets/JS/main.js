90ab82d464b3bfda7be6d23126fe11f90103c413
MartinFernandez <martinfer17@hotmail.com>
2022-07-02 04:26:28 -0300

    Push
    Se sube el portfolio a GitHub

---
+++ assets/JS/main.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".typed", {
+    strings: ['<b class="TextoMagico" style:> Progamador Full Stack JAVA </b>'],
+    typeSpeed: 75,
+    backSpeed: 90,
+    loop: true
+});
+typed
